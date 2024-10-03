2a37ad3b028248a0e1eae807211bc293ce39f003
SantoshChapagai <you@example.com>
2024-10-03 19:47:17 +0300

    update a form to share a experience

--- client/src/features/contact/Contact.tsx
+++ client/src/features/contact/Contact.tsx
@@ -4,28 +4,28 @@ export const Contact = () => {
         <form
           className='flex flex-col p-4 gap-4'
         >
-            <div className='flex flex-col'>
+            <div className='flex flex-col gap-2'>
               <label htmlFor='name'>Name</label>
               <input id='name' placeholder='Enter your full name'  className='p-2 rounded border'/>
             </div>
-            <div className='flex flex-col'>
+            <div className='flex flex-col gap-2'>
               <label htmlFor='country'>Country</label>
               <input id='country' placeholder='Country'  className='p-2 rounded border'/>
             </div>
-            <div className='flex flex-col'>
+            <div className='flex flex-col gap-2'>
               <label htmlFor='title'>Title</label>
               <input id='title' placeholder='Enter your experience heading'  className='p-2 rounded border'/>
             </div>
-            <div className='flex flex-col rounded'>
+            <div className='flex flex-col gap-2'>
               <label>Experience</label>
               <textarea
                 placeholder='Enter your experience'
                 rows={8}
-                className='border p-2 overflow-y-auto'
+                className='border p-2 overflow-y-auto rounded'
               />
             </div>
-            <div>
-                <button type='submit'>Submit</button>
+            <div className='text-center mt-2'>
+                <button type='submit' className='bg-sky-800 text-white p-2 rounded w-1/4'>Submit</button>
             </div>
         </form>
     </div>
---
+++ client/src/features/share/Share.tsx
@@ -0,0 +1,34 @@
+export const Share = () => {
+    return (
+      <div className='max-w-[820px] w-full mx-auto mt-16 bg-sky-300 rounded-xl'>
+          <form
+            className='flex flex-col p-4 gap-4'
+          >
+              <div className='flex flex-col gap-2'>
+                <label htmlFor='name'>Name</label>
+                <input id='name' placeholder='Enter your full name'  className='p-2 rounded border'/>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <label htmlFor='country'>Country</label>
+                <input id='country' placeholder='Country'  className='p-2 rounded border'/>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <label htmlFor='title'>Title</label>
+                <input id='title' placeholder='Enter your experience heading'  className='p-2 rounded border'/>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <label>Experience</label>
+                <textarea
+                  placeholder='Enter your experience'
+                  rows={8}
+                  className='border p-2 overflow-y-auto rounded'
+                />
+              </div>
+              <div className='text-center mt-2'>
+                  <button type='submit' className='bg-sky-800 text-white p-2 rounded w-1/4'>Submit</button>
+              </div>
+          </form>
+      </div>
+    )
+  }
+  
