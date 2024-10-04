422280c22ea5dc495e8f7b87503e443095d8872b
SantoshChapagai <you@example.com>
2024-10-04 21:09:21 +0300

    update the contact form

--- client/src/features/contact/Contact.tsx
+++ client/src/features/contact/Contact.tsx
@@ -2,25 +2,29 @@ export const Contact = () => {
   return (
     <div className='max-w-[820px] w-full mx-auto mt-16 bg-sky-300 rounded-xl'>
         <form
-          className='flex flex-col p-4 gap-4'
+          className='flex flex-col py-4 px-8 gap-4'
         >
             <div className='flex flex-col gap-2'>
               <label htmlFor='name'>Name</label>
               <input id='name' placeholder='Enter your full name'  className='p-2 rounded border'/>
             </div>
             <div className='flex flex-col gap-2'>
-              <label htmlFor='country'>Country</label>
-              <input id='country' placeholder='Country'  className='p-2 rounded border'/>
+              <label htmlFor='email'>Email</label>
+              <input id='cemail' placeholder='Email address'  className='p-2 rounded border'/>
             </div>
             <div className='flex flex-col gap-2'>
-              <label htmlFor='title'>Title</label>
-              <input id='title' placeholder='Enter your experience heading'  className='p-2 rounded border'/>
+              <label htmlFor='phone'>Phone number</label>
+              <input id='phone' placeholder='Your phone number'  className='p-2 rounded border'/>
             </div>
             <div className='flex flex-col gap-2'>
-              <label>Experience</label>
+              <label htmlFor='subject'>Subject</label>
+              <input id='subject' placeholder='Subject'  className='p-2 rounded border'/>
+            </div>
+            <div className='flex flex-col gap-2'>
+              <label>Message</label>
               <textarea
-                placeholder='Enter your experience'
-                rows={8}
+                placeholder='Write your message'
+                rows={5}
                 className='border p-2 overflow-y-auto rounded'
               />
             </div>
