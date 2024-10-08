ea135be36051dcc5f5b265e8d70a0f674c528928
SantoshChapagai <you@example.com>
2024-10-08 15:12:58 +0300

    update footer folor

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,6 +2,6 @@ export const Footer = () => {
   const date = new Date();
   const year = date.getFullYear();
   return (
-    <div className="text-center">&copy; Copyright Santosh {year}</div>
+    <div className="text-center bg-blue-200 py-4">&copy; Copyright Santosh {year}</div>
   )
 }
