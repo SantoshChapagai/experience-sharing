574cc46aa35a8e9780441da26ca0534425e7a131
SantoshChapagai <you@example.com>
2024-10-20 15:30:59 +0300

    fix the issue of formik in the signup form

--- client/src/features/signup/Signup.tsx
+++ client/src/features/signup/Signup.tsx
@@ -1,10 +1,10 @@
 import { Form, Formik } from "formik"
-import { SignupSchema } from "../../validation/signupSchema";
+import { SignupSchema, SignupSchemaType } from "../../validation/signupSchema";
 
 
 export const Signup = () => {
-    const handleSignup = () => {
-        console.log('hello');
+    const handleSignup = (values: SignupSchemaType) => {
+        console.log('sign up values', values);
     }
   return (
     <div className='max-w-[820px] w-full mx-auto mt-16 bg-sky-300 rounded-xl'>
@@ -21,27 +21,61 @@ export const Signup = () => {
           validationSchema={SignupSchema}
           onSubmit={handleSignup}
         >
+          {({getFieldProps, errors, touched}) => (
             <Form className='p-8 flex flex-col gap-4'>
               <div className='flex flex-col gap-2'>
                 <label htmlFor='firstName'>First Name</label>
-                <input id='firstName' placeholder='Enter your First name'  className='p-2 rounded border'/>
+                <input
+                  id='firstName'
+                  placeholder='Enter your First name'
+                  {...getFieldProps('firstName')}
+                  className='p-2 rounded border'
+                />
+                {errors.firstName && touched.firstName && (
+                <div className='text-red-500'>{errors.firstName}</div>
+                )}
               </div>
               <div className='flex flex-col gap-2'>
                 <label htmlFor='lastName'>Last Name</label>
-                <input id='lastName' placeholder='Enter your last name'  className='p-2 rounded border'/>
+                <input
+                  id='lastName'
+                  placeholder='Enter your last name'
+                  {...getFieldProps('lastName')}
+                  className='p-2 rounded border'
+                />
+                {errors.lastName && touched.lastName && (
+                <div className='text-red-500'>{errors.lastName}</div>
+                )}
               </div>
               <div className='flex flex-col gap-2'>
                 <label htmlFor='email'>Email</label>
-                <input id='cemail' placeholder='Email address'  className='p-2 rounded border'/>
+                <input
+                  id='email'
+                  placeholder='Email address'
+                  {...getFieldProps('email')}
+                  className='p-2 rounded border'
+                />
+                {errors.email && touched.email && (
+                <div className='text-red-500'>{errors.email}</div>
+                )}
               </div>
               <div className='flex flex-col gap-2'>
                 <label htmlFor='password'>Password</label>
-                <input type='password' id='password' placeholder='Your phone number'  className='p-2 rounded border'/>
+                <input
+                  id='password'
+                  placeholder='Your phone number'
+                  {...getFieldProps('password')}
+                  className='p-2 rounded border'
+                />
+                {errors.password && touched.password && (
+                <div className='text-red-500'>{errors.password}</div>
+                )}
               </div>
               <div className='text-center mt-4'>
                 <button type='submit' className='bg-sky-800 text-white p-2 rounded w-1/4'>Submit</button>
               </div>
             </Form>
+          )}
         </Formik>
     </div>
   )
