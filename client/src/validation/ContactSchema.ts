6dd7463710b5625ef601b036ad58f2b35e1eb8dd
SantoshChapagai <you@example.com>
2024-10-22 22:05:34 +0300

    solve formik issue in contact form

--- client/src/validation/ContactSchema.ts
+++ client/src/validation/ContactSchema.ts
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 export const ContactSchema = Yup.object({
     name: Yup.string().required('Name is required').min(8),
     email: Yup.string().email('Email is required'),
-    phoneNumber: Yup.string().optional(),
+    phone: Yup.string().optional(),
     subject: Yup.string().required('Provide the reason of contacting us'),
     message: Yup.string().required('Message is required')
 });
