e717be911ee1f52336c1c21439ef2e21abcfc421
SantoshChapagai <you@example.com>
2024-10-28 18:57:00 +0200

    fix the bug in different forms to make the email required

--- client/src/validation/signupSchema.ts
+++ client/src/validation/signupSchema.ts
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 export const SignupSchema = Yup.object({
     firstName: Yup.string().required('First name is required.').min(3),
     lastName: Yup.string().required('Last name is required.').min(3),
-    email: Yup.string().email('Email is required.'),
+    email: Yup.string().email().required('Email is required.'),
     password: Yup.string().required('Password is required').min(6)
 });
 
