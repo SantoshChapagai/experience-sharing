9f09858cba2254a0496d0c2b4113a84c50fe646f
SantoshChapagai <you@example.com>
2024-12-08 17:36:41 +0200

    update the acrd style

--- client/src/features/home/Home.tsx
+++ client/src/features/home/Home.tsx
@@ -3,7 +3,12 @@ import { Card } from "../../ui/Card";
 export const Home = () => {
   return (
     <div className="mt-10">
-      <Card />
+      <Card
+        author="Santosh"
+        imageSrc="@src/assets/santosh1.JPG"
+        heading="Old Nepal"
+        text="this is the awesome time that we are witnessing with the major of the hcudsgcu wdgcuywdgc hwsdgcywd hwdgywegd"
+      />
     </div>
   );
 };
