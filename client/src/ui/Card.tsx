cabf567b5ec5f6135a4fbc7961ffb2222fcf8b5f
SantoshChapagai <you@example.com>
2024-12-09 19:00:23 +0200

    update the style

--- client/src/ui/Card.tsx
+++ client/src/ui/Card.tsx
@@ -9,7 +9,7 @@ export const Card: React.FC<CardProps> = ({
   clickButton,
 }) => {
   return (
-    <div className="w-56 h-72 bg-neutral-30 rounded-lg p-2 flex flex-col gap-4">
+    <div className="w-56 h-fit bg-neutral-30 rounded-lg p-4 flex flex-col gap-4">
       <div className="flex flex-col gap-2">
         <h2 className="text-lg font-semibold">{heading}</h2>
         <p className="line-clamp-2 text-ellipsis text-sm">{text}</p>
